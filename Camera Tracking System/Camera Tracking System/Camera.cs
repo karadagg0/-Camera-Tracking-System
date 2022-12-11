545057aa967a0a9bc785f15f5026cf0b6c247105
karadagg0 <104911016+karadagg0@users.noreply.github.com>
2022-12-11 16:52:16 +0300

    Camera Tracking System
    Camera Tracking System Alghorithm

---
+++ Camera Tracking System/Camera Tracking System/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 target_offset;
+    void Start()
+    {
+        target_offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + target_offset, .125f);
+    }
+}
